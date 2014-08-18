c5d1266ea79f41d5af13104701ca91a3682501c6
Terence-Lee Davis <terencelee.davis@edelman.com>
2014-08-18 23:39:06 +0100

    Fixed invite command and adjusted the commands class to reflect name changes

--- Commands.php
+++ Commands.php
@@ -7,12 +7,12 @@ class Commands
 
     function __construct($sc)
     {
-        $this->wc = $sc;
+        $this->sc = $sc;
 
         $this->RegisterCommand("join", "Join", 0);
         $this->RegisterCommand("ping", "Ping", 0);
         $this->RegisterCommand("alias", "Alias", 1);
-        $this->RegisterCommand("invite", "Invite", 2);
+        $this->RegisterCommand("invite", "Invite", 1);
         $this->RegisterCommand("list", "ListUsers", 0); $this->RegisterCommand("online", "ListUsers", 0);
     }
 
@@ -24,13 +24,13 @@ class Commands
     function Join($sc, $from, $params)
     {
         $anonName = "Anon#" . rand(0, 100000);
-        $this->wc->SendSystemMessageTo("You have joined as " . $anonName . ". To change your name use .alias", $from);
+        $this->sc->SendSystemMessageTo("You have joined as " . $anonName . ". To change your name use .alias", $from);
 
         // Insert the user into the 'registered' users array
-        $this->wc->users[(string) $from] = array('alias' => $anonName);
+        $this->sc->users[(string) $from] = array('alias' => $anonName);
 
         // Breadcast the registration across the channel but exclude the person who joined
-        $this->wc->BroadcastSystemMessageExcl($anonName . " has joined the channel", array($from));
+        $this->sc->BroadcastSystemMessageExcl($anonName . " has joined the channel", array($from));
     }
 
     function Ping($sc, $from, $params)
@@ -43,31 +43,35 @@ class Commands
         // Check to see if it's a valid name or not
         if($params[1] == "")
         {
-            $this->wc->SendSystemMessageTo("Illegal name", $from);
+            $this->sc->SendSystemMessageTo("Illegal name", $from);
             return false;
         }
 
-        $oldName = $this->wc->users[$from]['alias'];
+        $oldName = $this->sc->users[$from]['alias'];
 
-        $this->wc->users[$from]['alias'] = $params[1];
-        $this->wc->SendSystemMessageTo("Your alias has been changed to ".$params[1], $from);
+        $this->sc->users[$from]['alias'] = $params[1];
+        $this->sc->SendSystemMessageTo("Your alias has been changed to ".$params[1], $from);
 
-        $this->wc->BroadcastSystemMessageExcl($oldName . " has changed his name to " . $params[1], array($from));
+        $this->sc->BroadcastSystemMessageExcl($oldName . " has changed his name to " . $params[1], array($from));
     }
 
     function Invite($sc, $from, $params)
     {
-        if(sizeof($params) == 2)
+        if(sizeof($params) >= 2)
         {
-            $this->wc->SendSystemMessageTo("You have been invited to chat on Seacloud by "
-            . $this->sc->users['from']['alias'] .
-            ". To join just type .join", $params[1]);
+            // Parse the parameter (Useful for copy pasta)
+            $number = str_replace(' ', '', $params[1]);
+            $number = str_replace('+', '', $number);
 
-            $this->wc->SendSystemMessageTo("You have sent an invite to " . $params[1], $from);
+            $this->sc->SendSystemMessageTo("You have been invited to chat on Seacloud by "
+            . $this->sc->users[$from]['alias'] .
+            ". To join just type .join", $number);
+
+            $this->sc->SendSystemMessageTo("You have sent an invite to " . $number, $from);
         }
         else
         {
-            $this->wc->SendSystemMessageTo("Usage: .invite <number>", $from);
+            $this->sc->SendSystemMessageTo("Usage: .invite <number>", $from);
         }
     }
 
